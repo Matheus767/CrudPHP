9d67dcd32e90530061931a3e0a929d37aca7ca07
Matheus767 <62734383+Matheus767@users.noreply.github.com>
2022-03-13 20:51:00 -0300

    Add files via upload

---
+++ crud-bootstrap-php/inc/footer.php
@@ -0,0 +1,15 @@
+</main> <!-- /container -->
+
+<hr>
+<footer class="container">
+    <p>&copy;2022 - Matheus Santos Castiço</p>
+</footer>
+
+<script scr="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+<script> window.jQuery || document.write('<script scr="<?php echo BASEURL; ?>js/jquery-1.11.2.min.js"><\/script>')</script>
+
+<script src="<?php echo BASEURL; ?>js/bootstrap.min.js"></script>
+
+<script src="<?php echo BASEURL; ?>js/main.js"></script>
+</body>
+</html>
